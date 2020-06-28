e36f483e0804f0a38817ef2180124e6a23e7195f
Tony Qu <tonyqus@gmail.com>
2020-06-29 06:41:54 +0800

    ASURADA.Web first checkin

---
+++ ASURADA/ASURADA.Web/Controllers/DatasourceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASURADA.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASURADA.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatasourceController : ControllerBase
+    {
+        public ActionResult<QueryResult> GetDatabases() 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
