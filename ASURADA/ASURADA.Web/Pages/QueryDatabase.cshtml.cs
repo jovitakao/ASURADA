713322b150f0cdbc2d4b1535bd1d1c869886246a
Tony Qu <tonyqus@gmail.com>
2020-07-09 06:59:07 +0800

    draw ui for querydatabase

---
+++ ASURADA/ASURADA.Web/Pages/QueryDatabase.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASURADA.Web.Pages
+{
+    public class QueryDatabaseModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
